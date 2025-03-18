6401e4248c028a2ed99d35b95831ebca13fd5ab5
Matthew Edward Scollick <matthew.scollick.022@pslib.cz>
2025-03-18 20:46:16 +0100

    test idk

--- GamebookCihullick.Server/Program.cs
+++ GamebookCihullick.Server/Program.cs
@@ -9,7 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+var dbPath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "WashingMachineDB.db");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite($"Data Source={dbPath}"));
+
+
+
+
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
