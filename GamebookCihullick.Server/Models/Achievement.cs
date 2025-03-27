8136d3b1c5b868173cc4ef220346662c9f47b194
Matthew Edward Scollick <matthew.scollick.022@pslib.cz>
2025-03-27 13:32:42 +0100

    new new new bean

--- GamebookCihullick.Server/Models/Achievement.cs
+++ GamebookCihullick.Server/Models/Achievement.cs
@@ -1,12 +1,15 @@
-﻿namespace GamebookCihullick.Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GamebookCihullick.Server.Models
 {
+    [Table("Achievements")]
     public class Achievement
     {
         public int AchievementID { get; set; }
         public int ImageID { get; set; }
-        public Image? Image { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-
+        public Image Image { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
